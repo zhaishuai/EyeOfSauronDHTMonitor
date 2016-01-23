1bbacdf6aa858cd30fc210fb7d4d6269dc897e35
zhaishuai <jszhaishuai@gmail.com>
2016-01-23 17:20:53 +0800

    立项

---
+++ EyeOfSauronDHTMonitor/src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  EyeOfSauronDHTMonitor
+//
+//  Created by shuaizhai on 1/23/16.
+//  Copyright © 2016 com.dhtMonitor.www. All rights reserved.
+//
+
+#include <iostream>
+#include <uv.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
